ad302f33f8dc47070825c19101301f62a7072057
andreitecsor <andrei.tecsor@gmail.com>
2020-09-28 19:28:04 +0300

    initial commit

---
+++ src/main/java/com/vaadin/tutorial/crm/backend/entity/AbstractEntity.java
@@ -0,0 +1,36 @@
+package com.vaadin.tutorial.crm.backend.entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class AbstractEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public boolean isPersisted() {
+        return this.id != null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AbstractEntity that = (AbstractEntity) o;
+
+        return id != null ? id.equals(that.id) : that.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
