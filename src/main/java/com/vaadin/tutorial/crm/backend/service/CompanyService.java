58bb2575bcbc57e811f5d9a853fa432f499bce83
andreitecsor <andrei.tecsor@gmail.com>
2020-10-05 22:49:36 +0300

    Added MainLayout + navigation + routing (+ little refactor)

--- src/main/java/com/vaadin/tutorial/crm/backend/service/CompanyService.java
+++ src/main/java/com/vaadin/tutorial/crm/backend/service/CompanyService.java
@@ -4,7 +4,9 @@ import com.vaadin.tutorial.crm.backend.entity.Company;
 import com.vaadin.tutorial.crm.backend.repository.CompanyRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class CompanyService {
@@ -14,7 +16,13 @@ public class CompanyService {
         this.companyRepository = companyRepository;
     }
 
-    public List<Company> findAll(){
+    public List<Company> findAll() {
         return companyRepository.findAll();
+    }
+
+    public Map<String, Integer> getStats() {
+         HashMap<String, Integer> stats = new HashMap<>();
+         findAll().forEach(company -> stats.put(company.getName(),company.getEmployees().size()));
+         return stats;
     }
 }
